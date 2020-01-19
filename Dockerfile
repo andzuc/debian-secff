e67c74f5ec059625b7966c837244d2c49e463d2f
Andrea Zuccherelli <andrea@zuccherelli.net>
2020-01-19 16:03:20 +0100

    rebased to debian:buster

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM debian:testing
+FROM debian:buster
 RUN apt-get -y update && apt-get install -y firefox-esr
 RUN useradd -m surfer
 USER surfer
 ENV HOME /home/surfer
 WORKDIR /home/surfer
+RUN chmod a+w .
 ENTRYPOINT ["/usr/bin/firefox", "--no-remote", "--profile", "/home/surfer/profile"]
--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-BASEDIR=$PWD
-PROFILE=$1
+BASEDIR="$PWD"
+PROFILE="$1"
 DOWNLOADS=/opt/vbox/media/shared/downloads
-ABSBASEDIR=$(realpath $BASEDIR)
-PROFILEDIR=${ABSBASEDIR}/secffprofile/${PROFILE}
+ABSBASEDIR="$(realpath $BASEDIR)"
+PROFILEDIR="${ABSBASEDIR}/secffprofile/${PROFILE}"
 LIBGL_ALWAYS_INDIRECT=1
 
 if [ -d "${PROFILEDIR}" ] ; then
-    docker run --name firefox-${PROFILE} --rm --net=host \
+    docker run --name "firefox-${PROFILE}" --rm --net=host \
 	   --ipc private \
 	   --env="DISPLAY" \
 	   -v "${HOME}/.Xauthority:/home/surfer/.Xauthority:rw" \
 	   -v "${PROFILEDIR}:/home/surfer/profile:rw" \
 	   -v "${DOWNLOADS}:/home/surfer/Downloads:rw" \
-	   firefox
+	   andzuc/debian-secff
 else
     echo "Profile not found: ${PROFILE}"
 fi
