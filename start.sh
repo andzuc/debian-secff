50bff2e5e7da50f7759a97412a639dc33e56e764
Andrea Zuccherelli <andrea@zuccherelli.net>
2023-01-29 22:34:14 +0100

    github build-image workflow

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ if [ -d "${PROFILEDIR}" ] ; then
 	   -v "${HOME}/.Xauthority:/home/surfer/.Xauthority:rw" \
 	   -v "${PROFILEDIR}:/home/surfer/profile:rw" \
 	   -v "${DOWNLOADS}:/home/surfer/Downloads:rw" \
-	   andzuc/debian-secff:2020012800
+	   andzuc/debian-secff:20230129
 else
     echo "Profile not found: ${PROFILE}"
 fi
